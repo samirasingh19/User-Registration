e4fcdcc9b069dd36cdcff557e3941d4cb4fd4dc5
samirasingh19 <samira.singh021@gmail.com>
2023-04-13 18:47:58 +0530

    Password3

---
+++ Password3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public class Password3
+    {
+        public static string PassWord = "^(?=.*[0-9])(?=.*[A-Z]).{8,}$";
+        public bool ValidatePassword(string password)
+        {
+            return Regex.IsMatch(password, PassWord);
+        }
+    }
+}
