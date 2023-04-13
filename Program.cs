f9ff7f0c4fd753f4bad6a5c76ecdbfed045b1f33
samirasingh19 <samira.singh021@gmail.com>
2023-04-13 18:42:52 +0530

    User Registration

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace UserRegistration
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the User Registration Page");
+            Password4 passWord = new Password4();
+            Console.WriteLine("Please enter password");
+            string password = Console.ReadLine();
+            Console.WriteLine(passWord.ValidatePassword(password));
+        }
+    }
+}
