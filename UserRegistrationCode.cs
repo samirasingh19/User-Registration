e11b24141de85180fcf4a2fef7bf10fff3cb608a
samirasingh19 <samira.singh021@gmail.com>
2023-04-13 18:49:38 +0530

    UserRegistrationCode

---
+++ UserRegistrationCode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public class UserRegistrationCode
+    {
+        public static string FirstName = "^[A-Z]{1}[A-Za-z]{2,}$";
+
+
+        public bool ValidateFName(string FName)
+        {
+            return Regex.IsMatch(FName, FirstName);
+        }
+    }
+}
