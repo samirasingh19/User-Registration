ee015b3e9146884fd52e6a2f88b37f1e4ab6887c
samirasingh19 <samira.singh021@gmail.com>
2023-04-13 18:44:49 +0530

    Email

---
+++ Email.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public class Email
+    {
+        public static string Email = "^[a-z0-9._+-]+@[a-z0-9.-]+\\.[a-z.]{2,}$";
+        public bool validateEmail(string email)
+        {
+            return Regex.IsMatch(email, Email);
+        }
+    }
+}
