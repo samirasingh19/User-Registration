d3c4353101cf182f8d7b417041595fb13ab85a85
samirasingh19 <samira.singh021@gmail.com>
2023-04-13 18:50:30 +0530

    LastName

---
+++ ValidLastName.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public class ValidLastName
+    {
+        public static string LastName = "^[A-Z]{1}[A-Za-z]{3,}$";
+        public bool ValidateLastName(string LName)
+        {
+            return Regex.IsMatch(LName, LastName);
+        }
+    }
+}
