949be9794e2d1b564b3f91ee79fe76ddd717f9b9
samirasingh19 <samira.singh021@gmail.com>
2023-04-13 18:45:35 +0530

    MobileNumber

---
+++ MobileNumber.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public class MobileNumber
+    {
+        public static string MobileNumber = "^[1-9]{1,3} [0-9]{10}$";
+        public bool ValidatMoNumber(string mobileNumber)
+        {
+            return Regex.IsMatch(mobileNumber, MobileNumber);
+        }
+    }
+}
